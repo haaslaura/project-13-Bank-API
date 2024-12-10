19709c6a46c3a98fb59da2d4ae0bdc78d5618ce8
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-12-10 13:30:31 +0100

    Correction on the server and installation of packages

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
-const dbConnection = require('./database/connection')
+// Ancienne ligne :
+// const dbConnection = require('./database/connection')
+// Correction :
+const dbConnection = () => require('./database/connection')
+
 
 dotEnv.config()
 
