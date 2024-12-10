57934d4c345ed626aa137c1ba9a4cc8441d0dbb5
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-12-10 16:20:52 +0100

    Installation of react and redux toolkit, start header & footer components

---
+++ argentbank-front/src/app/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from '../layouts/header/Header'
+import Footer from '../layouts/footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div>pouet</div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ argentbank-front/src/layouts/header/Header.jsx
@@ -0,0 +1,28 @@
+import './header.css'
+import logo from '../../assets/argentBankLogo.svg'
+
+const Header = () => {
+    
+    return (
+        <>
+            <nav class="main-nav">
+                <a class="main-nav-logo" href="./index.html">
+                    <img
+                        class="main-nav-logo-image"
+                        src={logo}
+                        alt="Argent Bank Logo"
+                    />
+                    <h1 class="sr-only">Argent Bank</h1>
+                </a>
+                <div>
+                    <a class="main-nav-item" href="./sign-in.html">
+                        <i class="fa fa-user-circle"></i>
+                        Sign In
+                    </a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
